63eafbf8e167e2df4f69a3912c80b479fee61712
Gekson <gekson@gmail.com>
2018-07-09 17:39:33 +0000

    Create class Position

---
+++ python/src/Position.py
@@ -0,0 +1,42 @@
+from Plateau import *
+
+class Position:
+
+    valid_directions = ('N', 'E', 'S', 'W')
+
+    def __init__(self, x, y, plateau):
+        self.plateau = plateau
+        self.coords = (x, y)
+
+    def X(self):
+        return self.coords[0]
+
+    def Y(self):
+        return self.coords[1]
+
+    def checkValidDirection(self, direction):
+        if direction not in Position.valid_directions:
+            raise "Invalid Direction Error. 'N', 'E', 'S', 'W'"
+    
+    def move(self, direction):
+        #print("MOVE******directions[0]",Position.valid_directions[0], "**************")
+        #print("******",self.coords[0], "**************")
+        #print("******",self.coords[1], "**************")
+        Position.checkValidDirection(self, direction)
+        
+        #COORDINATES - N E S W
+        coord_map = {
+            Position.valid_directions[0]: [self.coords[0], self.coords[1]+1], 
+            Position.valid_directions[1]: [self.coords[0]+1, self.coords[1]], 
+            Position.valid_directions[2]: [self.coords[0], self.coords[1]-1], 
+            Position.valid_directions[3]: [self.coords[0]-1, self.coords[1]]  
+        }
+        new_pos = coord_map.get(direction, None)
+        return Position.getPosition(self, new_pos[0], new_pos[1])
+
+    def getPosition(self, x, y):
+        #print("*****X=",x, " Y=",y, "*************")
+        #print("******",self.plateau.grid[0], " ",self.plateau.grid[1], "**************")
+        if (x < 0 or y < 0) or (x > self.plateau.grid[0] or y > self.plateau.grid[1]):
+            raise "Invalid Coordinate " 
+        return Position(x, y, self.plateau)
